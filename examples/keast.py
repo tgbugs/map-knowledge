932bc655e02e8c6334ac213dabc0f0f3d0a2569c
David Brooks <dave@bcs.co.nz>
2022-02-25 14:49:31 +1300

    Extend example.

--- examples/keast.py
+++ examples/keast.py
@@ -12,17 +12,18 @@ def print_knowledge(store, entity):
     print()
 
 if __name__ == '__main__':
-    store = KnowledgeStore('.')
+    store = KnowledgeStore(store_directory='.')
+
     print_knowledge(store, KEAST_MODEL)
-    for n in range(1, 4):
+    for n in [1, 5, 9, 11]:
         print_knowledge(store, KEAST_NEURON(n))
 
-    #  NLX    ## Add tests...
     print_knowledge(store, 'CL:0000540')
     print_knowledge(store, 'EMAPA:31526')
     print_knowledge(store, 'FMA:6541')
     print_knowledge(store, 'ILX:0777088')
     print_knowledge(store, 'NCBITaxon:10114')
     print_knowledge(store, 'UBERON:0002108')
+    print_knowledge(store, 'NLX:158005')
 
     store.close()
